c688897b621dc21269bf7fec2bc6fbc6d1cf0654
lucien <lucien.edel@geops.de>
2020-02-28 14:20:29 +0100

    Rounds coords to 4 decimals and handleMot keep fields value

--- src/Components/RoutingMenu/RoutingMenu.jsx
+++ src/Components/RoutingMenu/RoutingMenu.jsx
@@ -249,10 +249,6 @@ function RoutingMenu({ mots, stationSearchUrl, APIKey }) {
    */
   const handleMotChange = (event, newMot) => {
     setCurrentMotState(newMot);
-    setCurrentSearchResults([]);
-    setFocusedFieldIndex(0);
-    dispatch(setCurrentStops(['', '']));
-    dispatch(setCurrentStopsGeoJSON({}));
     dispatch(setCurrentMot(newMot));
   };
 
