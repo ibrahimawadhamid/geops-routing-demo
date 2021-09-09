7359f92445caf95cde9ac17f10061cebd17cfc7e
Eva Schiller | geOps <eva.schiller@geops.de>
2021-09-09 11:57:13 +0200

    Route information: Accept routes without surface_elevations...
    ...Currently relevant for geOps foot mot

--- src/Components/RouteInfosDialog/RouteInfosDialog.jsx
+++ src/Components/RouteInfosDialog/RouteInfosDialog.jsx
@@ -204,7 +204,9 @@ function RouteInfosDialog({
     const yArray = everyNth(coords, 3, 1);
     const altitudesArray = everyNth(coords, 3, 2).map(el => Math.round(el));
     const surfaceElevation = [].concat(
-      ...routes.map(r => r.get('surface_elevations').map(el => Math.round(el))),
+      ...routes.map(r =>
+        (r.get('surface_elevations') || []).map(el => Math.round(el)),
+      ),
     );
     setMinAltitude(Math.min(...surfaceElevation.concat(altitudesArray)));
     setMaxAltitude(Math.max(...surfaceElevation.concat(altitudesArray)));
