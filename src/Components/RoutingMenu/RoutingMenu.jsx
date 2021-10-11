22b515905d8cc3ef8624ce3978dfc0788426f00c
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2021-10-11 14:50:38 +0200

    Show and hide toggle button on enter event.

--- src/Components/RoutingMenu/RoutingMenu.jsx
+++ src/Components/RoutingMenu/RoutingMenu.jsx
@@ -209,6 +209,7 @@ function RoutingMenu({
   const [focusedFieldIndex, setFocusedFieldIndex] = useState(0);
   const [currentOtherMot, setCurrentOtherMot] = useState(undefined);
   const [menuVisible, setMenuVisible] = useState(true);
+  const [isEntered, setIsEntered] = useState(true);
 
   useEffect(() => {
     if (isRouteInfoOpen) {
@@ -627,6 +628,8 @@ function RoutingMenu({
         <Slide
           direction={isDesktop ? 'right' : 'down'}
           in={menuVisible}
+          onEntered={() => setIsEntered(true)}
+          onExiting={() => setIsEntered(false)}
           mountOnEnter
           unmountOnExit
         >
@@ -837,6 +840,7 @@ function RoutingMenu({
         </Slide>
         <button
           className="rd-routing-menu-toggle"
+          style={{ position: isEntered ? 'relative' : 'absolute' }}
           type="button"
           onClick={() => setMenuVisible(!menuVisible)}
         >
